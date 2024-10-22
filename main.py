d5e3555904687ab8c86d87ca316a6ad50acc9fe9
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2024-10-22 14:08:31 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# Ethan Lawrence 
+# Oct 22 2024
+# lists
+
+# 3-1 names
+names = ['Dax', 'Matthew', 'Trent']
+print(names[0])
+print(names[1])
+print(names[2])
+# 3-2 greetings
+print(f'Hello! {names[0]} Ready to present?')
+print(f'Hello! {names[1]} *franticly gesturing towards eachothers computers*')
+print(f'Hello! {names[2]} Crazy? I was crazy once...')
+# 3-3
+transport = ['Car', 'Walking', 'Skateboard']
+print(f'I go to school by {transport[0]}')
+print(f'When I am with my dog I am usually {transport[1]}')
+print(f'Because of my brother my whole family knows how to {transport[2]}')
